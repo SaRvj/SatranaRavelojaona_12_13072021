b91cb3140f9f33c2b2bcf8604d48556f5afbf0d4
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2022-04-29 17:55:23 +0200

    add Aside in router

--- satranaravelojaona_12_13072021/src/App.js
+++ satranaravelojaona_12_13072021/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Aside from "./components/Aside";
 import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
-      
+      <Aside />
       <Routes>
         
       </Routes>
