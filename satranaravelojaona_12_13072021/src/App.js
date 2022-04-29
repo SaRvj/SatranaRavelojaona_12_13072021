56523c7a7bbfa37848ebe11c64f8f13822970c6b
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2022-04-29 18:59:48 +0200

    add home in router

--- satranaravelojaona_12_13072021/src/App.js
+++ satranaravelojaona_12_13072021/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Aside from "./components/Aside";
 import Header from "./components/Header";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
@@ -9,7 +10,7 @@ export default function App() {
       <Header />
       <Aside />
       <Routes>
-        
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
